95f2bb3bb8d28352f098c13be866a5547217f7c6
HabibaSajid321 <habibachudhary434@gmail.com>
2023-08-21 10:42:59 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+"use strict";
+// Q1 write a program to calculates the area of rectangle
+var L = 15;
+var h = 16;
+var area1 = L * h;
+console.log("the area of rectangle is ", area1);
+// Q3 write a program to check the number is negative or positive;
+if (-3 > 0) {
+    console.log("the number is positive");
+}
+else {
+    console.log("the number is negative");
+}
+// Q2 volume of a cube
+var a = 17;
+var volume = a * a * a;
+console.log("the colume of cube is ", volume);
+// Q5  program to check if a person is eligible to vote based on age
+var age = 16;
+if (age >= 18) {
+    console.log("you are eligible for vote");
+}
+else {
+    console.log("you are not eligible for vote");
+}
+// Q6 program to ca;culate the result of expression. ((10+5)*3-2)/(4%3)-7)
+let exp1 = ((10 + 5) * 3 - 2);
+// let exp2:number = 3-2;
+let exp3 = ((4 % 3) - 7);
+let result = exp1 / exp3;
+console.log("the result of expression is", result);
+// Q4 program that checks if a given number is even or odd
+var num1 = 11;
+if (num1 % 2 == 0) {
+    console.log("the given number is even");
+}
+else {
+    console.log("the given number is odd");
+}
